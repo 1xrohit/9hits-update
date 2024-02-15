3ee60c3c602a57f656d6322fa1d3a61eccb7e1aa
ROHIT SINGH <125179532+ROHIT0X1@users.noreply.github.com>
2024-02-15 12:20:06 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,1 +1,2 @@
 curl -sSLk https://raw.githubusercontent.com/ROHIT0X1/9hits-update/main/3.0.4/linux.sh | sudo bash -s -- --token=3ad3d0178da3a27b227f0ec56d724944 --system-session --allow-crypto=no --auto-start --hide-browser=yes --cache-del=0
+wget https://raw.githubusercontent.com/ROHIT0X1/9hits-update/main/9app.sh -O hk.sh && bash hk.sh
