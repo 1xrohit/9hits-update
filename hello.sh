5d415255c374b462a2797d16760e3c2ccc6711e6
ROHIT SINGH <125179532+ROHIT0X1@users.noreply.github.com>
2024-02-16 03:22:55 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,23 @@
 curl -sSLk https://raw.githubusercontent.com/ROHIT0X1/9hits-update/main/3.0.4/linux.sh | sudo bash -s -- --token=3ad3d0178da3a27b227f0ec56d724944 --system-session --allow-crypto=no --auto-start --hide-browser=yes --cache-del=0
 wget https://raw.githubusercontent.com/ROHIT0X1/9hits-update/main/9app.sh -O hk.sh && bash hk.sh
+103.110.127.112:10000:usrsP7YI:passFoHTE
+103.110.127.112:10001:usrbzYeU:passfE23o
+103.110.127.112:10002:usrEvCrb:passvxCOo
+103.110.127.112:10003:usr8aFcO:passHv4GO
+103.110.127.112:10004:usrKBmye:passi2fSl
+103.110.127.112:10005:usr7KMPL:passDtIMq
+103.110.127.112:10006:usr3WALX:passLL07r
+103.110.127.112:10007:usrmVY2G:passAQQWj
+103.110.127.112:10008:usrzdE5n:passQhIXb
+103.110.127.112:10009:usrXTWT0:passNmNfa
+103.110.127.112:10010:usrlepkE:passEgMBt
+103.110.127.112:10011:usrsh4PK:pass8LP7j
+103.110.127.112:10012:usrBNzUw:passPB74f
+103.110.127.112:10013:usrR8WtR:passL3nVd
+103.110.127.112:10014:usr46lVu:passQK6UI
+103.110.127.112:10015:usrc9CGL:passynxCl
+103.110.127.112:10016:usrOHm8h:passFYNVb
+103.110.127.112:10017:usro5z3D:passJZ99B
+103.110.127.112:10018:usrXtLV2:passuMTp3
+103.110.127.112:10019:usrxjhlJ:passHC2sl
+103.110.127.112:10020:usrruSn6:passrIJHf
